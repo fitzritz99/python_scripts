1ab406927ed6107ed5ff26e1a8634fd9c206ed53
fitzritz99 <33161615+fitzritz99@users.noreply.github.com>
2018-07-21 10:05:29 -0700

    Update python_groupby_ex.py

--- python_groupby_ex.py
+++ python_groupby_ex.py
@@ -1,6 +1,23 @@
+import pandas as pd
+
+data = {'Country' : ['USA', 'USA', 'CAN', 'MEX', 'USA'],
+        'Value1' : [1, 2, 3, 4, 5],
+       'Value2': [10, 16, 15, 16, 16]}
+data_df = pd.DataFrame(data)
+#data_df
+
+count_unique = data_df.groupby(['Country','Value2']).size().groupby(level=0).agg({'ValueCount':'size'}).reset_index()
+
+count_by_group = data_df.groupby('Country')['Country'].agg({'Count':'count'}).reset_index()
+
+count_by_group = count_by_group.merge(count_unique,on=['Country'])
+print (count_by_group)
+
+
 print(df.groupby('Category').apply(lambda df,a,b: sum(df[a] * df[b]), 'Weight (oz.)', 'Quantity'))
 
 %%timeit -n 10
 for group, frame in df.groupby('STNAME'):
     avg = np.average(frame['CENSUS2010POP'])
     print('Counties in state ' + group + ' have an average population of ' + str(avg))
+    
